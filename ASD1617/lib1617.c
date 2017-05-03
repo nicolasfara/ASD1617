1812bca0aab919d5ddbbc2aab4fc1eaf3a72d640
nicolasfara <nicolas.farabegoli@icloud.com>
2017-05-03 23:46:58 +0200

    First commit

---
+++ ASD1617/lib1617.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lib1617.h"
+
+NODO * createFromFile(char * nameFile)
+{
+	return NULL;
+}
+
+void printDictionary(NODO * dictionary)
+{
+}
+
+int countWord(NODO * dictionary)
+{
+	return 0;
+}
+
+int insertWord(NODO ** dictionary, char * word)
+{
+	return 0;
+}
+
+int cancWord(NODO ** dictionary, char * word)
+{
+	return 0;
+}
+
+char * getWordAt(NODO * dictionary, int index)
+{
+	return NULL;
+}
+
+int insertDef(NODO * dictionary, char * word, char * def)
+{
+	return 0;
+}
+
+char * searchDef(NODO * dictionary, char * word)
+{
+	return NULL;
+}
+
+int saveDictionary(NODO * dictionary, char * fileOutput)
+{
+	return 0;
+}
+
+NODO * importDictionary(char * fileInput)
+{
+	return NULL;
+}
+
+int searchAdvance(NODO * dictionary, char * word, char ** primoRis, char ** secondoRis, char ** terzoRis)
+{
+	return 0;
+}
+
+int compressHuffman(NODO * dictionary, char * fileOutput)
+{
+	return 0;
+}
+
+int decompressHuffman(char * fileInput, NODO ** dictionary)
+{
+	return 0;
+}
---
+++ ASD1617/main1617.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lib1617.h"
+
+
+char* randomWord(int dim);
+char* randomString(int dim);
+
+int main(void) {
+	char* stringTemp;
+	srand(time(NULL));
+	
+	NODO* dictionary = createFromFile("fileTest1617.txt");
+
+	printf("\nSTAMPA DEL DIZIONARIO:\n");
+	printDictionary(dictionary);
+
+	printf("\nNumero di parole salvate nel dizionario : %d\n\n", countWord(dictionary));
+
+	inserisciWord(&dictionary, "tavolo");
+	inserisciWord(&dictionary, "b");
+	inserisciWord(&dictionary, "zoo");
+	
+	printf("\nNumero di parole salvate nel dizionario : %d\n\n", countWord(dictionary));
+	printf("\nSTAMPA DEL DIZIONARIO dopo 3 inserimenti:\n");
+	printDictionary(dictionary);
+
+	cancellaWord(&dictionary, getWordAt(dictionary, 5));
+	cancellaWord(&dictionary, getWordAt(dictionary, 1));
+	cancellaWord(&dictionary, getWordAt(dictionary, 0));
+	printf("\nSTAMPA DEL DIZIONARIO dopo 3 cancellazioni:\n");
+	printDictionary(dictionary);
+	
+	inserisciDef(dictionary, getWordAt(dictionary, 2), "una definizione");
+	inserisciDef(dictionary, getWordAt(dictionary, 4), "altra definizione");
+	printf("\nSTAMPA DEL DIZIONARIO dopo inserimento definizioni:\n");
+	printDictionary(dictionary);
+	printf("\nNumero di parole salvate nel dizionario : %d\n\n", countWord(dictionary));
+
+
+	printf("\nRicerca Parola \"%s\" -> definizione : [%s]\n\n", getWordAt(dictionary, 2), ricercaDef(dictionary, getWordAt(dictionary, 2)));
+	printf("\nRicerca Parola \"%s\" -> definizione : [%s]\n\n", getWordAt(dictionary, 7), ricercaDef(dictionary, getWordAt(dictionary, 7)));
+	stringTemp = "eftd";
+	printf("\nRicerca Parola \"%s\" -> definizione : [%s]\n\n", stringTemp, ricercaDef(dictionary, stringTemp));
+
+
+	system("PAUSE");
+	return (0);
+}
+
+char* randomWord(int dim) {
+	int r, i;
+
+	char* string = (char*)malloc((1 + dim) * sizeof(char));
+	if (string == NULL) return "random";
+
+	for (i = 0; i < dim; i++) {
+		string[i] = rand() % 25 + 97;
+	}
+	string[i] = '\0';
+	printf("\t\t-----> randomWord = %s \n", string);
+	return string;
+}
+
+char* randomString(int dim ) {
+	int r,i;
+	
+	char* string = (char*)malloc((1+dim) * sizeof(char));
+	if (string == NULL) return "random string";
+	
+	for ( i = 0; i < dim; i++) {
+		
+		r = rand()%40+97;
+		if (r >122) r = ' ';
+		string[i] = r;
+	}
+	string[i] = '\0';
+	printf("\t\t-----> randomString = %s \n", string);
+	return string;
+}
+
+
+
